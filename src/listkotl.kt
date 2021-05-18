9790ea080c1eef7dd5910200303022fbe453e5d1
jackline <cheptanuiJackline@gmail.comgit config --global user.email cheptanuiJackline@gmail.com>
2021-05-18 18:01:53 +0300

    first commit

---
+++ src/listkotl.kt
@@ -0,0 +1,45 @@
+fun main() {
+    var student=Book("thomas","blossom of the savannah",300)
+
+
+    println(digit(278))
+
+}
+
+data class Book(var author:String,var Title:String,var page:Int){
+
+
+fun F(:List<String>):List<String> {
+}
+
+
+}
+
+    fun digit(args: Array<String>) {
+        for (p in args)
+        val p = listOf(){
+        if (p%2==0)
+            if (p%2==300)
+                println("negative square")
+        do {
+            p++
+
+        } while (p%2==0)
+        println("positive square")
+
+    }
+
+}
+class Truck(var registration:Int,var Totalweight:Int,var capacity:Int)
+fun load(weight:Int){
+    load(weight:Int)
+    for (p in weight){
+        p++
+    }
+    println()
+
+}
+
+
+
+
